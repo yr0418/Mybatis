a3877dae3a9a603d8e6ce20a475cc0c49f9e034e
yr0418 <1933352594@qq.com>
2019-07-04 19:12:23 +0800

    更新数据库

--- src/main/resources/db/jsp.sql
+++ src/main/resources/db/jsp.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost_3306
+ Source Server         : mysql
  Source Server Type    : MySQL
  Source Server Version : 50562
  Source Host           : localhost:3306
@@ -11,11 +11,48 @@
  Target Server Version : 50562
  File Encoding         : 65001
 
- Date: 12/06/2019 01:07:15
+ Date: 04/07/2019 19:12:03
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for blog
+-- ----------------------------
+DROP TABLE IF EXISTS `blog`;
+CREATE TABLE `blog`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `u_id` int(11) DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `summary` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of blog
+-- ----------------------------
+INSERT INTO `blog` VALUES (1, 1, '我的世界', '你的爱情');
+INSERT INTO `blog` VALUES (2, 1, '我的天堂', '你的世界');
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `b_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES (1, '你在说啥', 1);
+INSERT INTO `comment` VALUES (2, '我的天啊', 1);
+INSERT INTO `comment` VALUES (3, '呜呜呜呜呜呜', 2);
+INSERT INTO `comment` VALUES (4, '哈哈哈哈哈哈哈', 2);
 
 -- ----------------------------
 -- Table structure for menu
@@ -26,7 +63,7 @@ CREATE TABLE `menu`  (
   `name` varchar(255) CHARACTER SET gbk COLLATE gbk_chinese_ci DEFAULT NULL,
   `price` double(10, 2) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of menu
@@ -40,6 +77,8 @@ INSERT INTO `menu` VALUES (6, '卤牛肉', 45.00);
 INSERT INTO `menu` VALUES (7, '皮蛋瘦肉粥', 3.00);
 INSERT INTO `menu` VALUES (8, '煲仔饭', 10.00);
 INSERT INTO `menu` VALUES (9, '玉米火腿炒饭', 8.00);
+INSERT INTO `menu` VALUES (14, '牛肉面', 12.00);
+INSERT INTO `menu` VALUES (20, '羊杂汤', 12.00);
 
 -- ----------------------------
 -- Table structure for reserve
@@ -54,14 +93,48 @@ CREATE TABLE `reserve`  (
   `address` varchar(255) CHARACTER SET gbk COLLATE gbk_chinese_ci DEFAULT NULL,
   `YN` varchar(255) CHARACTER SET gbk COLLATE gbk_chinese_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of reserve
 -- ----------------------------
-INSERT INTO `reserve` VALUES (5, '1', '1', 1.00, '2019-06-11 01:53:51', '1', '签收');
 INSERT INTO `reserve` VALUES (6, '123456', '红烧肉,辣椒炒肉,', 45.00, '2019-06-12 00:06:42', '3期公寓', '已签收');
 INSERT INTO `reserve` VALUES (7, '123456', '红烧排骨,红烧肉,辣椒炒肉,', 70.00, '2019-06-12 00:41:24', '3期公寓', '已签收');
+INSERT INTO `reserve` VALUES (8, 'root', '红烧排骨,红烧肉,', 50.00, '2019-06-20 18:14:26', '3期', '未签收');
+
+-- ----------------------------
+-- Table structure for u
+-- ----------------------------
+DROP TABLE IF EXISTS `u`;
+CREATE TABLE `u`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `status` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of u
+-- ----------------------------
+INSERT INTO `u` VALUES (1, '12344445555', '111111', 1);
+
+-- ----------------------------
+-- Table structure for u_details
+-- ----------------------------
+DROP TABLE IF EXISTS `u_details`;
+CREATE TABLE `u_details`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `u_id` int(10) DEFAULT NULL,
+  `nickname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of u_details
+-- ----------------------------
+INSERT INTO `u_details` VALUES (1, 1, '111', '111');
 
 -- ----------------------------
 -- Table structure for user
@@ -73,14 +146,18 @@ CREATE TABLE `user`  (
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `nickname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (1, 'root', 'root', '01');
 INSERT INTO `user` VALUES (3, 'test', 'test', '02');
-INSERT INTO `user` VALUES (11, '111', '111', '111');
-INSERT INTO `user` VALUES (12, '123456', '123456', '杨睿');
+INSERT INTO `user` VALUES (11, '111', '11234567', '杨睿');
+INSERT INTO `user` VALUES (12, '123456', '111', '111');
+INSERT INTO `user` VALUES (13, '22', '22', '王小旭');
+INSERT INTO `user` VALUES (14, '1234', '11234567', '杨睿');
+INSERT INTO `user` VALUES (15, '杨睿', '11234567', '杨睿');
+INSERT INTO `user` VALUES (16, '111', '111', '王睿');
 
 SET FOREIGN_KEY_CHECKS = 1;
